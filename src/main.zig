b1352293b0e3a83f3c5080a3d8ff69532f2d4c02
deforde <daniel.forde001@gmail.com>
2023-01-06 12:25:31 +0200

    implement basic command line arg handling

--- src/main.zig
+++ src/main.zig
@@ -17,17 +17,72 @@ const c = @cImport({
     @cInclude("unistd.h");
 });
 
-pub fn main() !void {
+const VER_MAJ: usize = 0;
+const VER_MIN: usize = 1;
+const VER_PATCH: usize = 0;
+
+fn usage() !void {
+    const usage_msg =
+        \\Usage: tmenu [options]
+        \\     Options:
+        \\         -f, --fullscreen        Run in full screen tui mode.
+        \\         -v, --version           Print the version number and exit.
+        \\         -h, --help              Print this usage message and exit.
+        \\
+    ;
+    const stdout = std.io.getStdOut().writer();
+    try stdout.writeAll(usage_msg);
+}
+
+fn reportArgError(arg: []u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Unrecognised argument: \"{s}\"\n", .{arg});
+    try usage();
+}
+
+fn printVersion() !void {
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{}.{}.{}\n", .{ VER_MAJ, VER_MIN, VER_PATCH });
+}
+
+pub fn main() !u8 {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
 
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    var fullscreen = false;
+
+    for (args[1..]) |arg| {
+        if (std.mem.eql(u8, arg, "--help") or std.mem.startsWith(u8, arg, "-h")) {
+            try usage();
+            return 0;
+        } else if (std.mem.eql(u8, arg, "--version") or std.mem.startsWith(u8, arg, "-v")) {
+            try printVersion();
+            return 0;
+        } else if (std.mem.eql(u8, arg, "--fullscreen") or std.mem.startsWith(u8, arg, "-f")) {
+            fullscreen = true;
+        } else {
+            try reportArgError(arg);
+            return 1;
+        }
+    }
+
     var entries = try EntryList.create(&allocator);
     defer entries.destroy();
 
-    // const select = try runFullscreenMenu(allocator, &entries);
-    const select = try runLightMenu(&entries);
+    const select = blk: {
+        if (fullscreen) {
+            break :blk try runFullscreenMenu(allocator, &entries);
+        }
+        break :blk try runLightMenu(&entries);
+    };
+
     if (select != null) {
         _ = c.execl(select.?.path.?.ptr, select.?.name.?.ptr, c.NULL);
     }
+
+    return 0;
 }
